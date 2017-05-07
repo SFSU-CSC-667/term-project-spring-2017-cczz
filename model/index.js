f0ec435dce6834aab4f0ac06ec431647da53de9c
Lijie Zhou <lijie@mail.sfsu.edu>
2017-05-06 18:41:09 -0700

    chat board completed

--- model/index.js
+++ model/index.js
@@ -12,12 +12,8 @@ module.exports = {
   	return db.one('SELECT * FROM users WHERE id = $1', id);
   },
 
-<<<<<<< HEAD
+
   getAllRooms: function() {
   	return db.any('SELECT * FROM rooms'); 
-=======
-  getAllRooms: function () {
-  	return db.any('SELECT * FROM rooms');
->>>>>>> 290a15c598330bfc05bcd1df02951ea85c268dfa
   }
 };

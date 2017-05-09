528d7d0bdb0c18701cdf2f7241df9cb6f119fa06
Xuan Zhang <xzhang8@mail.sfsu.edu>
2017-05-09 00:05:27 -0700

    register

--- model/index.js
+++ model/index.js
@@ -12,6 +12,15 @@ module.exports = {
   	return db.one('SELECT * FROM users WHERE id = $1', id);
   },
 
+  createUser: function (req) {
+  	//console.log(username)
+  	//console.log(req.body.username);
+  	//return db.none('INSERT INTO users(username, email, password) VALUES (\'t2\', \'t2@h\', \'123\')');
+  	return db.none('INSERT INTO users(username,email, password)' +
+  	 'VALUES (${username}, ${email}, ${password})', req.body);
+  	 //return db.none('INSERT INTO users(username, email, password) VALUES ($1, $2, $3)', [username, email, password] );
+  },
+
   getAllRooms: function () {
   	return db.any('SELECT * FROM rooms');
   }

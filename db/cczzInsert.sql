3fa5e1c76cb48423630fc597ca0d56e9901d917d
cli <daydreamerlee@gmail.com>
2017-05-16 10:57:37 -0700

    SQL CREATE modified to remove dummy room rows

--- db/cczzInsert.sql
+++ db/cczzInsert.sql
@@ -4,12 +4,6 @@ VALUES
   ('daydreamerlee', 'daydreamerlee@gmail.com', '456', 100, 2, './images/chip.png'),
   ('john', 'jrob@sfsu.edu', '456', 100, 3, './images/chip.png');
 
-INSERT INTO rooms (id, dealer_pid, small_blind, player_amount)
-VALUES
-	(1,1,100,200),
-	(2,2,200,400),
-	(3,3,300,600);
-
 INSERT INTO rounds (id, room_id, all_bet, current_bet, next_player, active_player_number)
 VALUES
 	(1, 1, 500, 10, 3, 4),
